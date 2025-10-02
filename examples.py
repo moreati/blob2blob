51fd4f77605bca055e7d7fd0012e540a60f47265
Alex Willmer <alex@moreati.org.uk>
2025-10-02 13:09:25 +0100

    Add gzip and xz compression examples
    The binary files are intentionally not committed to git, to minimize repo size
    and encourage reproducible examples. Their sha256 digests are stored.
    
    The inputs and variants should exercise known sources output differences.
    Ideally every method (cmd/file, cmd/stdio, lib) will be able to generate the
    same set of variants. This is not yet the case for xz; existing Python
    bindings to liblzma don't cover multithreaded compression.

---
+++ examples.py
@@ -0,0 +1,327 @@
+#!/usr/bin/env python3
+"""
+Write a directory tree of compression examples with metadata
+"""
+
+import abc
+import argparse
+import json
+import gzip
+import hashlib
+import io
+import lzma
+import os
+import pathlib
+import platform
+import subprocess
+import sys
+import tempfile
+import types
+import typing
+import zlib
+
+INPUTS = {
+    '0-empty': lambda: b'',
+    '1-a': lambda: b'a',
+    '1e1-a': lambda: b'a' * 10,
+    '1e2-a': lambda: b'a' * 100,
+    '1e3-a': lambda: b'a' * 1000,
+    '1e6-a': lambda: b'a' * 1000_000,
+    '1e8-a': lambda: b'a' * 100_000_000,
+}
+
+INFGEN_ARGS = [
+    '-dd',  # Write bit pattern of each element
+    '-i',  # Write gzip or zlib header info
+]
+
+
+class BaseCmd(abc.ABC):
+    common_args: list[str]
+    variants: dict[str, list[str]]
+    extension: str
+    format: str
+
+    def __init__(self, cmd: str):
+        self.cmd = cmd
+
+    def version(self):
+        res = subprocess.run(
+            [self.cmd, '--version'],
+            stdin=subprocess.DEVNULL,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            encoding='ascii',
+            check=True,
+        )
+        if res.stdout:
+            return res.stdout
+        return res.stderr
+
+    def compress_file(self, filename: os.PathLike, variant: str):
+        result = subprocess.run(
+            [self.cmd, *self.common_args, *self.variants[variant], filename],
+            stdin=subprocess.DEVNULL,
+            capture_output=True,
+            encoding='ascii',
+            check=True,
+        )
+        out_file_path = pathlib.Path(f'{filename}.{self.extension}')
+        compressed_data = out_file_path.read_bytes()
+        out_file_path.unlink()
+
+        info = dict(
+            args=result.args[:-1] + ['<filename>'],
+            stdout=result.stdout,
+            stderr=result.stderr,
+            returncode=result.returncode,
+        )
+        return compressed_data, info
+
+    def compress_stdio(self, data: bytes, variant: str):
+        result = subprocess.run(
+            [self.cmd, *self.common_args, *self.variants[variant]],
+            input=data,
+            capture_output=True,
+            check=True,
+        )
+        info = dict(
+            args=result.args,
+            stderr=result.stderr.decode('ascii'),
+            returncode=result.returncode,
+        )
+        return result.stdout, info
+
+
+class GzipCmd(BaseCmd):
+    common_args = [
+        '--keep',
+        '--no-name',  # Suppress filename & timestamp in header
+    ]
+    variants = {
+        'fastest': ['-1'],
+        'default': [],
+        'best': ['-9'],
+    }
+    extension = 'gz'
+    format = 'gzip'
+
+
+class XzCmd(BaseCmd):
+    common_args = [
+        '--keep',
+    ]
+    variants = {
+        'mt1-fastest': ['-0', '--threads=+1'],
+        'mt1-default': ['--threads=+1'],
+        'mt1-best': ['-9', '--threads=+1'],
+        'mt1-best-extreme': ['-9e', '--threads=+1'],
+        'mt2-fastest': ['-0', '--threads=+1'],
+        'mt2-default': ['--threads=2'],
+        'mt2-best': ['-9', '--threads=2'],
+        'mt2-best-extreme': ['-9e', '--threads=2'],
+        'st-fastest': ['-0', '--threads=1'],
+        'st-default': ['--threads=1'],
+        'st-best': ['-9', '--threads=1'],
+        'st-best-extreme': ['-9e', '--threads=1'],
+    }
+    extension = 'xz'
+    format = 'xz'
+
+
+class BaseLib(abc.ABC):
+    common_args: dict[str, typing.Any]
+    variants: dict[str, dict[str, typing.Any]]
+    extension: str
+    format: str
+
+    def __init__(self, mod: types.ModuleType):
+        self.mod = mod
+
+    def compress(self, data: bytes, variant: str):
+        args = self.common_args | self.variants[variant]
+        compressed_data = self.mod.compress(data, **args)
+        info = {'args': args, 'module': self.mod.__name__}
+        return compressed_data, info
+
+
+class GzipLib(BaseLib):
+    common_args = {'mtime': 0}
+    variants = {
+        'fastest': {'compresslevel': 1},
+        'default': {},
+        'best': {'compresslevel': 9},
+    }
+    extension = 'gz'
+    format = 'gzip'
+
+
+class XzLib(BaseLib):
+    common_args = {}
+    variants = {
+        'st-fastest': {'preset': 0},
+        'st-default': {},
+        'st-best': {'preset': 9},
+        'st-best-extreme': {'preset': 9 | lzma.PRESET_EXTREME},
+    }
+    extension = 'xz'
+    format = 'xz'
+
+
+def environment_info():
+    info = {}
+    uname = platform.uname()
+    info.update(
+        platform=dict(
+            machine=platform.machine(),
+            release=platform.release(),
+            system=platform.system(),
+            uname=dict(
+                machine=uname.machine,
+                processor=uname.processor,
+                release=uname.release,
+                system=uname.system,
+                version=uname.version,
+            ),
+            version=platform.version(),
+        ),
+        python=dict(
+            stdlib=dict(
+                sys=dict(
+                    maxsize=sys.maxsize,
+                    version=sys.version,
+                    version_info=sys.version_info,
+                ),
+                zlib=dict(
+                    ZLIB_VERSION=zlib.ZLIB_VERSION,
+                    ZLIB_RUNTIME_VERSION=zlib.ZLIB_RUNTIME_VERSION,
+                ),
+            ),
+
+        ),
+    )
+    return info
+
+
+def infgen(infgen_cmd: str, gzip_data: bytes):
+    return subprocess.run(
+        [infgen_cmd] + INFGEN_ARGS,
+        input=gzip_data,
+        capture_output=True,
+        check=True,
+    )
+
+
+def write_example(impl, out_dir: os.PathLike, name: str, data: bytes, info: dict) -> tuple[pathlib.Path, pathlib.Path]:
+    out_dir = pathlib.Path(out_dir)
+    out_dir.mkdir(parents=True, exist_ok=True)
+    data_path = out_dir / f'{name}.{impl.extension}'
+    data_path.write_bytes(data)
+    info_path = out_dir / f'{name}.json'
+    info_path.write_text(json.dumps(info, indent=2, sort_keys=True))
+    return data_path, info_path
+
+
+def file_sha256(path: os.PathLike):
+    hasher = hashlib.sha256()
+    with open(path, 'rb') as f:
+        while chunk := f.read(io.DEFAULT_BUFFER_SIZE):
+            hasher.update(chunk)
+    return hasher
+
+
+def write_sha256sums(out_dir: os.PathLike, relative_names: list[os.PathLike]):
+    out_dir = pathlib.Path(out_dir)
+    hashers = [file_sha256(out_dir / name) for name in relative_names]
+    with open(out_dir / 'sha256sums', 'w', encoding='utf-8') as f:
+        for name, hasher in zip(relative_names, hashers):
+            f.write(f'{hasher.hexdigest()} *{name}\n')
+
+
+def main():
+    if sys.version_info < (3, 13):
+        sys.exit('Python 3.13 or higher is required.')
+
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    group = parser.add_argument_group('commands')
+    group.add_argument(
+        '--gzip-cmd',
+        default='gzip',
+        type=GzipCmd,
+        metavar='CMD',
+    )
+    group.add_argument('--infgen-cmd', default='infgen', metavar='CMD')
+    group.add_argument(
+        '--xz-cmd',
+        default='xz',
+        type=XzCmd,
+        metavar='CMD',
+    )
+
+    parser.add_argument(
+        '--output-dir',
+        default='examples/generated',
+        metavar='PATH',
+        type=pathlib.Path,
+        help='Directory to write output to',
+    )
+    # parser.add_argument("name", help="Name to give this set of examples")
+    args = parser.parse_args()
+
+    output_base: pathlib.Path = args.output_dir
+    output_base.mkdir(parents=True, exist_ok=True)
+
+    env_info_path = output_base / 'environment-info.json'
+    env_info_path.write_text(
+        json.dumps(environment_info(), indent=2, sort_keys=True),
+    )
+
+    inputs = {name: fn() for name, fn in INPUTS.items()}
+    cmds: list[BaseCmd] = [args.gzip_cmd, args.xz_cmd]
+    libs: list[BaseLib] = [GzipLib(gzip), XzLib(lzma)]
+
+    # cmd.compress_file() examples, uncompressed data provided in a named file
+    with tempfile.TemporaryDirectory() as tmpdir:
+        for in_name, in_data in inputs.items():
+            in_path = pathlib.Path(tmpdir, in_name)
+            in_path.write_bytes(in_data)
+
+        for cmd in cmds:
+            for variant in cmd.variants:
+                out_dir = output_base / cmd.format / 'cmd_file' / variant
+                data_paths: list[pathlib.Path] = []
+                for in_name in inputs:
+                    in_path = pathlib.Path(tmpdir, in_name)
+                    out_data, out_info = cmd.compress_file(in_path, variant)
+                    data_path, _ = write_example(cmd, out_dir, in_name, out_data, out_info)
+                    data_paths.append(data_path)
+                write_sha256sums(out_dir, [p.name for p in data_paths])
+
+    # cmd.compress_stdio() examples, uncompressed data provided to stdin
+    for cmd in cmds:
+        for variant in cmd.variants:
+            out_dir = output_base / cmd.format / 'cmd_stdio' / variant
+            data_paths: list[pathlib.Path] = []
+            for in_name, in_data in inputs.items():
+                out_data, out_info = cmd.compress_stdio(in_data, variant)
+                data_path, _  = write_example(cmd, out_dir, in_name, out_data, out_info)
+                data_paths.append(data_path)
+            write_sha256sums(out_dir, [p.name for p in data_paths])
+
+    # lib.compress() examples, uncompressed data provided as a byte string
+    for lib in libs:
+        for variant in lib.variants:
+            out_dir = output_base / lib.format / 'py_stdlib' / variant
+            data_paths: list[pathlib.Path] = []
+            for in_name, in_data in inputs.items():
+                out_data, out_info = lib.compress(in_data, variant)
+                data_path, _  = write_example(lib, out_dir, in_name, out_data, out_info)
+                data_paths.append(data_path)
+            write_sha256sums(out_dir, [p.name for p in data_paths])
+
+
+if __name__ == '__main__':
+    main()
